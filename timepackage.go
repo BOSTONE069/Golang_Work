5142f857771bb2106f47a1dedf1b9eae7bbbdbba
rhemney <bostoneochieng69@gmail.com>
2022-11-24 04:56:09 -0800

    Time management in Golang programming language

--- timepackage.go
+++ timepackage.go
@@ -15,5 +15,7 @@ func main() {
 	//This is time usage with specified default formats in Golang
 	fmt.Println(presentTime.Format("01-02-2006 15:04:05 Monday"))
 
-	
+	createdTime := time.Date(2022, time.November, 24, 4, 34, 0, 0, time.UTC)
+	fmt.Println(createdTime)
+	fmt.Println(createdTime.Format("01-02-2006 Monday"))
 }
