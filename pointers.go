846f2e5be808c79126adee15a276aea6e8107b86
rhemney <bostoneochieng69@gmail.com>
2022-11-25 04:40:00 -0800

    Modified the pointers file

--- pointers.go
+++ pointers.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("Welcome to a class on pointers")
+
 	star := "Polaris"
-	
+
 	// Add your code below:
   starAddress := &star
   fmt.Println("The address of star is", starAddress)
+	fmt.Println("The value of star is", *starAddress)
 }
