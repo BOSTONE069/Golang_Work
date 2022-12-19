c7a03f861a35f65fcd5f7826547d28d654637c2a
rhemney <bostoneochieng69@gmail.com>
2022-12-19 03:53:11 -0800

    Added the comments to the various pats of the code

--- elseifstatement.go
+++ elseifstatement.go
@@ -5,6 +5,9 @@ import "fmt"
 func main() {
 	amountStolen := 64650
 
+	// Checking if the amount stolen is greater than 1 million, if it is, it prints "We've hit the
+	// jackpot!" If it is not, it checks if the amount stolen is greater than or equal to 5000, if it is,
+	// it prints "Think of all the candy we can buy". If it is not, it prints "Why did we even do this?"
 	if amountStolen > 1000000 {
 		fmt.Println("We've hit the jackpot!")
   } else if amountStolen >= 5000{
